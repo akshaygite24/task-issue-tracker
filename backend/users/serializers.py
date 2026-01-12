03c8ed98c0e6ae971fcd45bb285e0230eafcca3f
akshaygite24 <akshaygite2004@gmail.com>
2026-01-12 19:52:08 +0530

    Add Login for user

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -17,4 +17,20 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
     
 
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
 
+
+    def validate(self, data):
+        user = authenticate(
+            username=data.get("username"),
+            password=data.get("password"),
+        )
+
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        
+        data["user"] = user
+        return data
+    
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 
 
 class RegisterView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         serializers = RegisterSerializer(data=request.data)
 
@@ -28,3 +30,24 @@ class RegisterView(APIView):
                 )
         
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response(
+            {
+                "access": str(refresh.access_token),
+                "refresh": str(refresh),
+            },
+            status=status.HTTP_200_OK,
+            )
+    
